82c45770b5ac23a8f36fa0f3c8b6949d47fa7a74
Daniel Flores - DicDev <desarrollo@dicora.com>
2023-09-29 12:41:49 -0600

    node - intro

---
+++ 01intro/app.js
@@ -0,0 +1,11 @@
+const amount = 12;
+
+if(amount < 10) {
+  console.log("small number");
+} else if (amount < 20) {
+  console.log("medium number");
+} else {
+  console.log("large number");
+}
+
+console.log('Hey my first nade app!!!');
